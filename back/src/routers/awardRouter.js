4a8199c4e273ac537274ed768c11e38b99a8fae3
[AI5/2반] 한혜진 <ljehhj2632@gmail.com>
2022-08-30 15:39:35 +0900

    api endpoint 수정

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -5,7 +5,7 @@ import { Award } from "../db";
 
 const awardRouter = Router();
 
-awardRouter.post("/award/create", async (req, res, next) => {
+awardRouter.post("/award", async (req, res, next) => {
   try {
     if (is.emptyObject(req.body)) {
       throw new Error(
@@ -46,7 +46,7 @@ awardRouter.post("/award/create", async (req, res, next) => {
 awardRouter.put("/awards/:id", async (req, res, next) => {
   try {
     // award의 user_id와 요청을 보낸 user의 id가 동일한지 체크!!!
-    // if (award.user_id.id !== req.user.id) {
+    // if (award.user_id !== req.currentUserId) {
     //   throw new Error("Not Authorized");
     // }
 
@@ -81,35 +81,29 @@ awardRouter.put("/awards/:id", async (req, res, next) => {
 });
 
 // 특정 user의 모든 수상내역 get
-awardRouter.get(
-  "/awardlist/:user_id",
-  async (req, res, next) => {
-    try {
-      const user_id = req.params.user_id;
-      const userAwardInfo = await awardService.getAwardInfo({ user_id });
+awardRouter.get("/users/:user_id/awards", async (req, res, next) => {
+  try {
+    const user_id = req.params.user_id;
+    const userAwardInfo = await awardService.getAwardInfo({ user_id });
 
-      if (userAwardInfo.errorMessage) {
-        throw new Error(userAwardInfo.errorMessage);
-      }
-      res.status(200).json(userAwardInfo);
-    } catch (error) {
-      next(error);
+    if (userAwardInfo.errorMessage) {
+      throw new Error(userAwardInfo.errorMessage);
     }
+    res.status(200).json(userAwardInfo);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
-awardRouter.delete(
-  "/awards/:id/delete",
-  async (req, res, next) => {
-    try {
-      const award_id = req.params.id;
-      await awardService.deleteAward({ award_id })
+awardRouter.delete("/awards/:id", async (req, res, next) => {
+  try {
+    const award_id = req.params.id;
+    await awardService.deleteAward({ award_id });
 
-      res.sendStatus(204);
-    } catch (error) {
-      next(error);
-    }
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 export { awardRouter };
