cde05edb148203e804f2e77b3a1c4a0acd9bd0c1
임 지원 <9universe9th@gmail.com>
2022-09-01 11:45:05 +0000

    backserver url 변경

--- front/src/config.js
+++ front/src/config.js
@@ -1,3 +1,3 @@
-export const backServer = "http://localhost:";
-//export const backServer = "http://kdt-ai5-team12.elicecoding.com:";
+//export const backServer = "http://localhost:";
+export const backServer = "http://kdt-ai5-team12.elicecoding.com:";
 export const backPort = 5001;
